de33b6c8a706c67366760e3fed323776f359b981
Nouman Zahid <151716630+NoumanZahid-85@users.noreply.github.com>
2025-11-23 13:15:59 +0500

    Time: 7 ms (19.72%) | Memory: 23.3 MB (79.17%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,45 +1,70 @@
 class MinStack {
 public:
-    stack <long long int> s;
-    long long int minVal; 
-
-    MinStack() {
-        
-    }
+    // 1st Solution with Space Complexity O(2*n)
+    stack<pair<int, int>> s;
     
     void push(int val) {
         if(s.empty()){
-            s.push(val);
-            minVal = val;
-        }else{
-            if(val < minVal){
-                s.push((long long)2*val - minVal);
-                minVal = val;
-            }else{
-                s.push(val);
-            }
+            s.push({val, val});
+        } else {
+            int minVal = min(val, s.top().second);
+            s.push({val, minVal});
         }
     }
     
     void pop() {
-        if(s.top() < minVal){
-            minVal = minVal*2 - s.top();
-        }
         s.pop();
-        
     }
     
-    int top() {
-        if(s.top() < minVal){
-            return minVal;
-        }
-        return s.top();
+    int top() {  // Fixed return type
+        return s.top().first;
     }
     
     int getMin() {
-        return minVal;
+        return s.top().second;
     }
 };
+
+    // 2nd Solution is efficient and it remove the need of storing the minVal all time.
+
+    // stack <long long int> s;
+    // long long int minVal; 
+    // MinStack() {
+    // }
+    
+    // void push(int val) {
+    //     if(s.empty()){
+    //         s.push(val);
+    //         minVal = val;
+    //     }else{
+    //         if(val < minVal){
+    //             s.push((long long)2*val - minVal);
+    //             minVal = val;
+    //         }else{
+    //             s.push(val);
+    //         }
+    //     }
+    // }
+    
+    // void pop() {
+    //     if(s.top() < minVal){
+    //         minVal = minVal*2 - s.top();
+    //     }
+    //     s.pop();
+        
+    // }
+    
+    // int top() {
+    //     if(s.top() < minVal){
+    //         return minVal;
+    //     }
+    //     return s.top();
+    // }
+    
+    // int getMin() {
+    //     return minVal;
+    // }
+// };
 
 /**
  * Your MinStack object will be instantiated and called as such:
