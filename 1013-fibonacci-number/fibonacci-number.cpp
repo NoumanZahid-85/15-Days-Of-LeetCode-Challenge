380d3e2f34131afdf93981399c30f3ae03fa1d83
Nouman Zahid <151716630+NoumanZahid-85@users.noreply.github.com>
2025-11-27 01:17:01 +0500

    Time: 3 ms (63.27%) | Memory: 8.2 MB (11.48%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,20 +1,39 @@
-class Solution {
+// class Solution {
+// public:
+//     // Method number 1, through memoization.
+//     // Add & for reference 
+//     int dp(int n, vector<int>& OneDArray){
+//         if(n <= 1){
+//             return n;
+//         }
+//         if(OneDArray[n] != -1){
+//             return OneDArray[n];
+//         }
+//         return OneDArray[n] = dp(n-1, OneDArray) + dp(n-2, OneDArray);
+//     }
+//     int fib(int n) {
+//         if(n <= 1)
+//             return n;
+//         // I have used 1D-Array bcz only one variable is changed here (n).    
+//         vector<int> OneDArray(n+1, -1);
+//         return dp(n, OneDArray);
+//     }
+// };
+
+    // Method 2 through bottomUp approach using an Array as extra space.
+class Solution{
 public:
-    // Add & for reference 
-    int dp(int n, vector<int>& OneDArray){
+    int fib(int n){
         if(n <= 1){
             return n;
         }
-        if(OneDArray[n] != -1){
-            return OneDArray[n];
-        }
-        return OneDArray[n] = dp(n-1, OneDArray) + dp(n-2, OneDArray);
-    }
-    int fib(int n) {
-        if(n <= 1)
-            return n;
-        // I have used 1D-Array bcz only one variable is changed here (n).    
         vector<int> OneDArray(n+1, -1);
-        return dp(n, OneDArray);
+        // OneDArray[i] is ith fibonacci number. 
+        OneDArray[0] = 0;
+        OneDArray[1] = 1;
+        for(int i = 2; i <= n; i++){
+            OneDArray[i] = OneDArray[i-1] + OneDArray[i-2];
+        }
+        return OneDArray[n];
     }
 };
