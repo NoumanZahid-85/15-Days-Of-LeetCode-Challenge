ef4c19992496af9bc2afdb231a6c314075b67416
Nouman Zahid <151716630+NoumanZahid-85@users.noreply.github.com>
2025-11-27 01:24:08 +0500

    Time: 0 ms (100.00%) | Memory: 7.7 MB (87.02%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -21,19 +21,39 @@
 // };
 
     // Method 2 through bottomUp approach using an Array as extra space.
-class Solution{
+// class Solution{
+// public:
+//     int fib(int n){
+//         if(n <= 1){
+//             return n;
+//         }
+//         vector<int> OneDArray(n+1, -1);
+//         // OneDArray[i] is ith fibonacci number. 
+//         OneDArray[0] = 0;
+//         OneDArray[1] = 1;
+//         for(int i = 2; i <= n; i++){
+//             OneDArray[i] = OneDArray[i-1] + OneDArray[i-2];
+//         }
+//         return OneDArray[n];
+//     }
+// };
+
+
+// Now lets solve this in constant space complexity.
+class Solution {
 public:
-    int fib(int n){
-        if(n <= 1){
+    int fib(int n) {
+        if (n <= 1)
             return n;
+
+        int c;
+        int a = 0, b = 1;
+
+        for (int i = 1; i < n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
         }
-        vector<int> OneDArray(n+1, -1);
-        // OneDArray[i] is ith fibonacci number. 
-        OneDArray[0] = 0;
-        OneDArray[1] = 1;
-        for(int i = 2; i <= n; i++){
-            OneDArray[i] = OneDArray[i-1] + OneDArray[i-2];
-        }
-        return OneDArray[n];
+        return c;
     }
 };
