14cd0b4a230faf02841e55bb394b4a283d6f9588
Nouman Zahid <151716630+NoumanZahid-85@users.noreply.github.com>
2025-12-01 21:18:07 +0500

    Time: 0 ms (100.00%) | Memory: 11.9 MB (15.79%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int n = s.length();
+        if (n <= 1) return 0;
+        
+        vector<int> dp(n, 0);
+        int maxLen = 0;
+        
+        for (int i = 1; i < n; i++) {
+            if (s[i] == ')') {
+                if (s[i-1] == '(') {
+                    // Case: "...()"
+                    dp[i] = (i >= 2 ? dp[i-2] : 0) + 2;
+                } 
+                else if (i - dp[i-1] > 0 && s[i - dp[i-1] - 1] == '(') {
+                    // Case: "...))" and we have matching '(' before
+                    dp[i] = dp[i-1] + 2;
+                    if (i - dp[i-1] - 2 >= 0) {
+                        dp[i] += dp[i - dp[i-1] - 2];
+                    }
+                }
+                maxLen = max(maxLen, dp[i]);
+            }
+        }
+        return maxLen;
+    }
+};
