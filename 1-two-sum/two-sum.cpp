c2f0ba20b1ec7e4a1f8bec3c85c4d0b899ff8265
Nouman Zahid <151716630+NoumanZahid-85@users.noreply.github.com>
2026-01-18 22:25:50 +0500

    Time: 3 ms (67.64%) | Memory: 14.9 MB (19.53%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
-    std::vector<int> twoSum(std::vector<int>& nums, int target) {
-        for (int i = 0; i < (int)nums.size(); ++i) {
-            for (int j = i + 1; j < (int)nums.size(); ++j) {
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        // Its TC is O(n) not O(n**2) like in double loop.
+        // Make the extra map to store current number with index to use in future.
+        map<int, int> mp;
+        for(int i = 0; i<n; i++){
+            int remaining = target - nums[i];
+            if(mp.find(remaining) != mp.end()){
+                return {mp[remaining], i};
             }
+            mp[nums[i]] = i;
         }
-        return {}; // No solution found
+        return {};
     }
 };
